3317b858517617b5c46f09764c073ee301a606cf
Michael Mace <michael.mace@conexon.us>
2024-05-28 14:21:43 -0500

    fix prettier config

--- server.mjs
+++ server.mjs
@@ -1,44 +1,43 @@
-import { createServer } from "node:http";
-import next from "next";
-import { Server } from "socket.io";
+import { createServer } from 'node:http'
+import next from 'next'
+import { Server } from 'socket.io'
 
-const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3000
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
-const handler = app.getRequestHandler();
+const app = next({ dev, hostname, port })
+const handler = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const httpServer = createServer(handler);
+  const httpServer = createServer(handler)
 
-  const io = new Server(httpServer);
+  const io = new Server(httpServer)
 
-  io.on("connection", (socket) => {
+  io.on('connection', (socket) => {
     console.log('a client is connected')
 
-    
     socket.on('message', (data) => {
       socket.emit('return:message', 'this is the return message')
-      console.log("👀 🔍 ~ io.on ~ socket.id:", socket.id)
-      console.log("👀 🔍 ~ io.on ~ socket.handshake:", socket.handshake)
-      console.log("👀 🔍 ~ io.on ~ SET socket.rooms:", socket.rooms)
-      socket.join("room1");
-      console.log("👀 🔍 ~ socket.on ~ data:", data)
-      console.log("👀 🔍 ~ io.on ~ socket.rooms:", socket.rooms)
+      console.log('👀 🔍 ~ io.on ~ socket.id:', socket.id)
+      console.log('👀 🔍 ~ io.on ~ socket.handshake:', socket.handshake)
+      console.log('👀 🔍 ~ io.on ~ SET socket.rooms:', socket.rooms)
+      socket.join('room1')
+      console.log('👀 🔍 ~ socket.on ~ data:', data)
+      console.log('👀 🔍 ~ io.on ~ socket.rooms:', socket.rooms)
     })
-    
+
     socket.on('disconnect', () => {
       console.log('a client disconnected')
     })
-  });
+  })
 
   httpServer
-    .once("error", (err) => {
-      console.error(err);
-      process.exit(1);
+    .once('error', (err) => {
+      console.error(err)
+      process.exit(1)
     })
     .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
-    });
-});
+      console.log(`> Ready on http://${hostname}:${port}`)
+    })
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,39 @@
-import { useEffect, useState } from "react";
-import { socket } from "../socket";
+import { useEffect, useState } from 'react'
+import { socket } from '../socket'
 
 export default function Home() {
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState("N/A");
+  const [isConnected, setIsConnected] = useState(false)
+  const [transport, setTransport] = useState('N/A')
   const [message, setMessage] = useState(null)
 
   useEffect(() => {
     if (socket.connected) {
-      onConnect();
+      onConnect()
     }
 
     function onConnect() {
       console.log('client socket.id', socket.id)
-      setIsConnected(true);
-      setTransport(socket.io.engine.transport.name);
+      setIsConnected(true)
+      setTransport(socket.io.engine.transport.name)
 
-      socket.io.engine.on("upgrade", (transport) => {
-        setTransport(transport.name);
-      });
+      socket.io.engine.on('upgrade', (transport) => {
+        setTransport(transport.name)
+      })
     }
 
     function onDisconnect() {
-      setIsConnected(false);
-      setTransport("N/A");
+      setIsConnected(false)
+      setTransport('N/A')
     }
 
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
+    socket.on('connect', onConnect)
+    socket.on('disconnect', onDisconnect)
 
     return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-    };
-  }, []);
-
-
+      socket.off('connect', onConnect)
+      socket.off('disconnect', onDisconnect)
+    }
+  }, [])
 
   async function submitMessage() {
     socket.emit('message', message)
@@ -46,11 +44,18 @@ export default function Home() {
 
   return (
     <div>
-      <p>Status: { isConnected ? "connected" : "disconnected" }</p>
-      <p>Transport: { transport }</p>
+      <p>Status: {isConnected ? 'connected' : 'disconnected'}</p>
+      <p>Transport: {transport}</p>
       <div>
-      <input onChange={(e) => setMessage(e.target.value)} type='text' className="text-black"></input>
-      <button className="border border-white" onClick={()=> submitMessage()}>submit</button></div>
+        <input
+          onChange={(e) => setMessage(e.target.value)}
+          type="text"
+          className="text-black"
+        ></input>
+        <button className="border border-white" onClick={() => submitMessage()}>
+          submit
+        </button>
+      </div>
     </div>
-  );
+  )
 }
